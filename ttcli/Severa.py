4fdde8313831f759557c66adcc4d7d1747a83c72
Bjørn Snoen <bjorn.snoen@gmail.com>
2021-09-13 10:44:57 +0200

    (feat) Montly reports, fix week rollover in severa

--- ttcli/Severa.py
+++ ttcli/Severa.py
@@ -1,5 +1,6 @@
 from datetime import date, datetime, timedelta
-from json import loads, dumps
+from dateutil.relativedelta import relativedelta
+from json import loads
 from os import environ, getenv
 from typing import Optional, List, Dict
 
@@ -178,7 +179,7 @@ class Severa(ApiClient):
     def get_logged_during_week(self, week: int):
         year = date.today().year
         starting_datetime = datetime.strptime(f"{year}-W{week}-1", "%Y-W%W-%w")
-        ending_datetime = starting_datetime + timedelta(days=7)
+        ending_datetime = starting_datetime + timedelta(days=6)
 
         return loads(self.api_get(self.user_endpoint("/workhours"), params={
             "firstRow": 0,
@@ -195,10 +196,10 @@ def severa_command():
     pass
 
 
-@severa_command.command()
-@click.argument("week", type=int, default=datetime.today().isocalendar()[1])
-def timesheet(week: int):
-    client = Severa()
+def timesheet(week: int, client: Optional[Severa]) -> List[Dict]:
+    if not client:
+        client = Severa()
+
     result: List[Dict] = client.get_logged_during_week(week)
     result.sort(key=lambda entry: entry.get('eventDate'))
     for entry in result:
@@ -212,3 +213,41 @@ def timesheet(week: int):
         click.secho(f'{hours}: ', fg="yellow" if hours == 7.5 else "red", nl=False)
         click.secho(description)
         click.secho('--', fg='bright_black')
+
+    click.secho(f"Total w{week}: ", fg="green", nl=False)
+    click.secho(f'{sum([float(entry["quantity"]) for entry in result])}h')
+    return result
+
+
+@severa_command.command(name="timesheet")
+@click.argument("week", type=int, default=datetime.today().isocalendar()[1])
+def timesheet_week(week: int):
+    timesheet(week, Severa())
+
+@severa_command.command()
+@click.argument("month", type=int, default=datetime.today().month)
+def timesheet_month(month: int):
+    client = Severa()
+    year = datetime.today().year
+    first_day = datetime.strptime(f"{year}-{month}-1", "%Y-%m-%d")
+    last_day = first_day + relativedelta(months=1, days=-1)
+
+    weeks = []
+
+    for week in range(int(first_day.strftime('%W')), int(last_day.strftime('%W')) + 1):
+        result = list(filter(lambda entry: date.fromisoformat(entry["eventDate"]).month == month, timesheet(week, client)))
+        if len(result):
+            weeks.append(result)
+        click.secho("--\n", fg='bright_black')
+
+
+    month_total = 0
+    for result in weeks:
+        click.secho(f"Total w{date.fromisoformat(result[0]['eventDate']).strftime('%W')}: ", fg="green", nl=False)
+        week_total = sum([float(entry["quantity"]) for entry in result])
+        month_total += week_total
+        click.secho(f'{week_total}h')
+
+    click.secho("--", fg='bright_black')
+    click.secho(f"Total {first_day.strftime('%b')}: ", fg="green", nl=False)
+    click.secho(f"{month_total}h")
